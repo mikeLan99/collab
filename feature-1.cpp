6120eaee12432263c10619463d042809378a6fad
Kabeer Joshi <89746908+kabeerx9@users.noreply.github.com>
2023-05-10 22:10:09 +0530

    Update feature-1.cpp

--- feature-1.cpp
+++ feature-1.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 using namespace std;
 
+// kabeer added this , i had to add two numbers in  this program 
+
 int main() {
-    cout << "Hello, World!" << endl;
+    int x = 10;
+    int y = 20;
+    cout<<"Sum is :"<<x+y<<endl;
     return 0;
 }
