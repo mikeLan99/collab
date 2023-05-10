3ed68eafaee450fbc8d69ce2318cbbb8e34d1a0d
CoderBoiiiiiiii <132659589+CoderBoiiiiiiii@users.noreply.github.com>
2023-05-10 22:21:26 +0530

    Update feature-2.py

--- feature-2.py
+++ feature-2.py
@@ -1,9 +1,9 @@
 #person 2 - added this feature change:
-#we have to sum two float numbers only 
+#we have to multiply two float numbers only 
 
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
 
-diff = num1 - num2
+product = num1 * num2
 
-print(f"The sum of {num1} and {num2} is {diff}")
+print(f"The sum of {num1} and {num2} is {product}")
