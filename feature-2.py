66f0059158fdb2d473fbbfce5a433168c396dfae
CoderBoiiiiiiii <132659589+CoderBoiiiiiiii@users.noreply.github.com>
2023-05-10 22:03:58 +0530

    Integer addition in python

--- feature-2.py
+++ feature-2.py
@@ -1,5 +1,8 @@
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
+#person 2 - added this feature change:
+#we have to sum two integer numbers not float 
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
 
 sum = num1 + num2
 
