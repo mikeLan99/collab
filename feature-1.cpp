88dea47fc60869dce64626cae3e4f6cdf5f41a65
CoderBoiiiiiiii <132659589+CoderBoiiiiiiii@users.noreply.github.com>
2023-05-10 22:11:37 +0530

    Update feature-1.cpp

--- feature-1.cpp
+++ feature-1.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 using namespace std;
 
-// kabeer added this , i had to add two numbers in  this program 
+//  subtraction has to be done  
 
 int main() {
-    int x = 10;
-    int y = 20;
-    cout<<"Sum is :"<<x+y<<endl;
+    int x = 20;
+    int y = 40;
+    cout<<"Subtraction is :"<<x-y<<endl;
     return 0;
 }
