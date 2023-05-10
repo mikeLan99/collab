17a4226c50a070046cd237f5a0ec78cd1cec94ca
Kabeer Joshi <89746908+kabeerx9@users.noreply.github.com>
2023-05-10 22:32:50 +0530

    sorting added

--- feature-1.cpp
+++ feature-1.cpp
@@ -4,6 +4,11 @@ using namespace std;
 // multiplication has to be done 
 
 int main() {
+
+    // adding sort an array code 
+    vector<int>v = {1,5,2,3};
+    sort(v.begin(),v.end());
+
     int x = 10;
     int y = 20;
     cout<<"Multiplication is :"<<x*y<<endl;
--- feature-2.py
+++ feature-2.py
@@ -1,6 +1,12 @@
 #person 2 - added this feature change:
 #we have to multiply two float numbers only 
 
+
+#adding sorting code 
+nums = [1,5,2,3]
+nums.sort()
+print(nums)
+
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
 
